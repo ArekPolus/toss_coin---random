b0a1a1d65349569e67d6aa5b01e47d6345e25879
ArekPolus <72617381+ArekPolus@users.noreply.github.com>
2022-01-02 13:55:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+coin = random.randint(0, 1)
+
+if coin == 1:
+  print(coin)
+  print("heads")
+else:
+  print(coin)
+  print("tails")
+
+
+
+
+
+
+
